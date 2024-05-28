eed2d3819ca0af8e987fe39bfd43d927a64c3101
aleksbal <aleksandar.balaban@gmail.com>
2024-05-28 16:20:26 +0200

    Head component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    flowbite.content(),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    flowbite.plugin(),
+  ],
+};
